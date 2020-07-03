3ef1fbf66434b1aba991d3d6c40104444967e8ba
lolieatapple <lolieatapple@gmail.com>
2020-07-03 11:57:59 +0800

    finish

---
+++ config.js
@@ -0,0 +1,9 @@
+const API_KEY = "";
+const SEC_KEY = "";
+const dingdingBotUrl = "";
+
+module.exports = {
+    API_KEY,
+    SEC_KEY,
+    dingdingBotUrl
+}
--- pos-bad.js
+++ pos-bad.js
@@ -1,18 +1,20 @@
 const ApiInstance = require('iwan-sdk');
+const schedule = require('node-schedule');
+const axios = require('axios');
+const { API_KEY, SEC_KEY, dingdingBotUrl } = require('./config');
 
 //In order to get an YourApiKey and YourSecretKey, sign up at iWan. Then create a new project to get a new YourApiKey and YourSecretKey key pair.
-let YourApiKey = "ae8a283e999e4fbba7ea54d6692806efea33b51bf797128a32d3d352d7a214e2";
-let YourSecretKey = "7897dde450229bb1da66c8cda21639d95b0248949ad48c1b618f99f2b7e23c31";
+let YourApiKey = API_KEY;
+let YourSecretKey = SEC_KEY;
 
 //Subject to https://iwan.wanchain.org
 let option = {
-  url: "api2.wanchain.org",
+  url: "api.wanchain.org",
   port: 8443,
   flag: "ws",
   version: "v3"
 };
 
-let apiTest = new ApiInstance(YourApiKey, YourSecretKey, option);
 
 let testArray = [
   ['getRegisteredValidator', 'WAN', '0x0288c83219701766197373d1149f616c62b52a7d'],
@@ -23,38 +25,73 @@ let addresses = {};
 
 async function main() {
   try {
+    let apiTest = new ApiInstance(YourApiKey, YourSecretKey, option);
     let missed = [];
-    for(let i=18350; i<=18388; i++) {
+    let epochId = await apiTest.getEpochID('WAN');
+    console.log('current epoch:', epochId);
+    for (let i = epochId - 2; i <= epochId; i++) {
       let ret = await apiTest['getValidatorActivity']('WAN', i);
       if (!ret) {
         break;
       }
 
-      for (let m=0;  m<ret.epActivity.length; m++) {
+      for (let m = 0; m < ret.epActivity.length; m++) {
         if (ret.epActivity[m] == 0) {
           missed.push([i, ret.epLeader[m], 'EL', ret.epActivity[m]]);
         }
       }
 
-      for (let m=0;  m<ret.rpActivity.length; m++) {
+      for (let m = 0; m < ret.rpActivity.length; m++) {
         if (ret.rpActivity[m] == 0) {
           missed.push([i, ret.rpLeader[m], 'RNP', ret.rpActivity[m]]);
         }
       }
     }
-    
-    for (let i=0; i<missed.length; i++) {
+
+    let msg = "检测到POS活跃度异常节点！当前epochID是" + epochId + "\n" + "最近3天内活跃异常节点信息如下：\n";
+
+    for (let i = 0; i < missed.length; i++) {
       let info = await apiTest.getRegisteredValidator(missed[i][1]);
-      console.log(...missed[i], info[0]?info[0].name:'none');
+      console.log(...missed[i], info[0] ? info[0].name : 'none');
+      let title = missed[i].toString();
+      let name = info[0] ? info[0].name : 'none';
+      msg += title;
+      msg += " " + name;
+      msg += "\n";
     }
 
+    if (missed.length > 0) {
+      console.log(msg);
+      await dingdingSend(msg);
+    }
+
+
     apiTest.close();
-    process.exit(0);
   } catch (err) {
     console.log(err);
     apiTest.close();
-    process.exit(1);
   }
 }
 
-main();
+const dingdingSend = async (msg) => {
+  let format = {
+    "msgtype": "text",
+    "text": {
+      "content": msg
+    },
+  };
+  let ret = await axios.post(dingdingBotUrl, format);
+  console.log(ret.data);
+}
+
+// * * * * * *
+// second minute hour day month dayOfWeek
+const robotSchedules = () => {
+  // update: The settlement robot calls this function daily to update the capital pool and settle the pending refund.
+  schedule.scheduleJob('*/10 * * * * *', async () => {
+    let msg = await main();
+    console.log(msg);
+  });
+}
+
+robotSchedules();
